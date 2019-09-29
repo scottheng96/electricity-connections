62de23afd938815eaf267a8812106e48a55267ca
Scott Heng On <sdh45@duke.edu>
2019-09-29 15:32:45 -0400

    changed number of neighbors from 7 to 10

--- transmission_classification.py
+++ transmission_classification.py
@@ -23,19 +23,19 @@ plt.close()
 Set directory parameters
 '''
 # Set the directories for the data and the CSV files that contain ids/labels
-base_dir = '/Users/kylebradbury/Documents/Projects/kaggle_data'
-dir_train_images  = path.join(base_dir, 'train')
-dir_test_images   = path.join(base_dir, 'test')
+base_dir = '/Users/scottheng/Documents/Courses/Fall_2019/ENERGY_795/electricity-connections'
+dir_train_images  = path.join(base_dir, 'train/train')
+dir_test_images   = path.join(base_dir, 'test/test')
 dir_train_labels  = path.join(base_dir, 'train.csv')
 dir_test_ids      = path.join(base_dir, 'sample_submission.csv')
 
 '''
-Include the functions used for loading, preprocessing, features extraction, 
+Include the functions used for loading, preprocessing, features extraction,
 classification, and performance evaluation
 '''
 
 def load_data(dir_data, dir_labels, training=True):
-    ''' Load each of the image files into memory 
+    ''' Load each of the image files into memory
 
     While this is feasible with a smaller dataset, for larger datasets,
     not all the images would be able to be loaded into memory
@@ -45,7 +45,7 @@ def load_data(dir_data, dir_labels, training=True):
     labels_pd = pd.read_csv(dir_labels)
     ids       = labels_pd.id.values
     data      = []
-    
+
     for identifier in ids:
         fname     = path.join(dir_data, identifier)
         image     = mpl.image.imread(fname)
@@ -56,50 +56,50 @@ def load_data(dir_data, dir_labels, training=True):
         return data, labels
     else:
         return data, ids
-    
+
 
 def preprocess_and_extract_features(data):
     '''Preprocess data and extract features
-    
+
     Preprocess: normalize, scale, repair
     Extract features: transformations and dimensionality reduction
     '''
     # Here, we do something trivially simple: we take the average of the RGB
     # values to produce a grey image, transform that into a vector, then
     # extract the mean and standard deviation as features.
-    
+
     # Make the image grayscale
     data = np.mean(data, axis=3)
-    
+
     # Vectorize the grayscale matrices
     vectorized_data = data.reshape(data.shape[0],-1)
-    
+
     # extract the mean and standard deviation of each sample as features
     feature_mean = np.mean(vectorized_data,axis=1)
     feature_std  = np.std(vectorized_data,axis=1)
-    
+
     # Combine the extracted features into a single feature vector
     features = np.stack((feature_mean,feature_std),axis=-1)
-    
+
     return features
 
 def set_classifier():
     '''Shared function to select the classifier for both performance evaluation
     and testing
     '''
-    return KNeighborsClassifier(n_neighbors=7)
+    return KNeighborsClassifier(n_neighbors=10)
 
 def cv_performance_assessment(X,y,k,clf):
     '''Cross validated performance assessment
-    
+
     X   = training data
     y   = training labels
     k   = number of folds for cross validation
     clf = classifier to use
-    
-    Divide the training data into k folds of training and validation data. 
+
+    Divide the training data into k folds of training and validation data.
     For each fold the classifier will be trained on the training data and
-    tested on the validation data. The classifier prediction scores are 
+    tested on the validation data. The classifier prediction scores are
     aggregated and output
     '''
     # Establish the k folds
@@ -110,15 +110,15 @@ def cv_performance_assessment(X,y,k,clf):
         # Extract the training and validation data for this fold
         X_train, X_val   = X[train_index], X[val_index]
         y_train          = y[train_index]
-        
+
         # Train the classifier
         X_train_features = preprocess_and_extract_features(X_train)
         clf              = clf.fit(X_train_features,y_train)
-        
+
         # Test the classifier on the validation data for this fold
         X_val_features   = preprocess_and_extract_features(X_val)
         cpred            = clf.predict_proba(X_val_features)
-        
+
         # Save the predictions for this fold
         prediction_scores[val_index] = cpred[:,1]
         print('Training Fold {} of {} Complete'.format(i,k))
@@ -129,7 +129,7 @@ def plot_roc(labels, prediction_scores):
     fpr, tpr, _ = metrics.roc_curve(labels, prediction_scores, pos_label=1)
     auc = metrics.roc_auc_score(labels, prediction_scores)
     legend_string = 'AUC = {:0.3f}'.format(auc)
-   
+
     plt.plot([0,1],[0,1],'--', color='gray', label='Chance')
     plt.plot(fpr, tpr, label=legend_string)
     plt.xlabel('False Positive Rate')
@@ -164,7 +164,7 @@ plot_roc(labels, prediction_scores)
 Sample script for producing a Kaggle submission
 '''
 
-produce_submission = False # Switch this to True when you're ready to create a submission for Kaggle
+produce_submission = True # Switch this to True when you're ready to create a submission for Kaggle
 
 if produce_submission:
     # Load data, extract features, and train the classifier on the training data
